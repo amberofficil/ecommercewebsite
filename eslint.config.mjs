6926fdc4377929c4da2829e1860d7d6078e0153e
amber officil <amberofficial380@gmail.com>
2025-01-10 17:21:50 +0500

    ecommercewebsite

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,5 +12,12 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
+{
+
+"extends"; ["next/core-web-vitals", "next/typscript"],
+"rules";{"@typescript-eslint/no-explicit-any";"off",
+  "@typescript-eslint/no-unused-vars";"off"
+}
+}
 
 export default eslintConfig;
